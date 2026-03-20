0f17e4c594730e32944b20fcbdcafa2a4935617a
gutslips <127069897+gutslips@users.noreply.github.com>
2026-03-20 17:13:55 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -3,6 +3,5 @@
 //  index.html 과 같은 폴더에 두세요
 // ─────────────────────────────────────────
 window.APP_CONFIG = {
-  // Google Cloud Console > 사용자 인증 정보 > OAuth 2.0 클라이언트 ID
   clientId: '764317576361-k25ir8cg376tmhr95cht7e0dukdaivrd.apps.googleusercontent.com'
 };
