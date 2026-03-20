ee6541d0497368b8523315d2c27866e8c1a87570
gutslips <127069897+gutslips@users.noreply.github.com>
2026-03-20 17:06:29 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,5 @@
 window.APP_CONFIG = {
   // Google Cloud Console > 사용자 인증 정보 > OAuth 2.0 클라이언트 ID
   clientId: '764317576361-k25ir8cg376tmhr95cht7e0dukdaivrd.apps.googleusercontent.com';
-const FIXED_CLIENT_ID = '764317576361-k25ir8cg376tmhr95cht7e0dukdaivrd.apps.googleusercontent.com';  // ← 본인 Client ID로 교체
 
 };
